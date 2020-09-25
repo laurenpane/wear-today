62f9d0e67361a01fbd34c8ecbf7feca97f06c978
Lauren Pane <lauren.pane1@googlemail.com>
2020-09-25 15:35:13 +0100

    Coordinates on frontend but not posting to backend

--- src/Components/GetGeoLocation.js
+++ src/Components/GetGeoLocation.js
@@ -4,12 +4,22 @@ export default function GetGeoLocation() {
   return (
     <div>
       {"geolocation" in navigator
-        ? navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position.coords);
+        ? navigator.geolocation.getCurrentPosition(async (position) => {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
             document.getElementById("latitude").textContent = lat;
             document.getElementById("longitude").textContent = lon;
+
+            const data = { lat, lon };
+            const options = {
+              method: "POST",
+              body: JSON.stringify(data),
+              headers: { "Content-Type": "application/json" },
+            };
+
+            const response = await fetch("/api/geo", options);
+            const json = await response.json();
+            console.log(json);
           })
         : console.log("Unavailable")}
       <p>
--- src/server.js
+++ src/server.js
@@ -1,4 +1,27 @@
+require("dotenv").config();
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-app.listen(3001, () => console.log("Listening at 3001"));
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3001;
+const path = require("path");
+
+app.post("/api/geo", (req, res) => {
+  console.log(req);
+  //   console.log(req.body);
+  //   const data = req.body;
+  //   res.json({
+  //     status: "success",
+  //     latitude: data.lat,
+  //     longitude: data.lon,
+  //   });
+});
+
+// app.use(express.static(path.join(__dirname, "../build")));
+
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../build/index.html"));
+// });
+
+app.listen(PORT, () => {
+  console.log(`API running on http://localhost:${PORT}`);
+});
